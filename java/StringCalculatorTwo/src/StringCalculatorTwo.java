4d80f7a136db8d65dac66ea53fe94fe219374230
Sebastian <haloz@gmx.net>
2013-11-17 23:09:07 +0100

    convert as own method

--- java/StringCalculatorTwo/src/StringCalculatorTwo.java
+++ java/StringCalculatorTwo/src/StringCalculatorTwo.java
@@ -9,23 +9,26 @@ import static java.util.Arrays.asList;
  * Created with IntelliJ IDEA.
  * User: intellibook
  * Date: 17.11.13
- * Time: 21:15
+* Time: 21:15
  * To change this template use File | Settings | File Templates.
  */
 public class StringCalculatorTwo {
+
+    public List<Integer> convertToNumbers(String numbers) {
+        return convert(numbers.split(","), new Converter<String, Integer>() {
+            @Override
+            public Integer convert(String from) {
+                return Integer.parseInt(from);
+            }
+        });
+    }
 
     public int Add(String numbers) {
         if(numbers.isEmpty()) {
             return 0;
         }
 
-        List<Integer> convertedNumbers = convert(numbers.split(","), new Converter<String, Integer>() {
-            @Override
-            public Integer convert(String from) {
-                return Integer.parseInt(from);
-            }
-        });
-
+        List<Integer> convertedNumbers = convertToNumbers(numbers);
 
         return sum(convertedNumbers).intValue();
     }
